c5ec541aff8b0a69a8cd1f8a395ea65ace12134e
ParnKrub <parn2544krub@gmail.com>
2022-08-31 17:17:22 +0700

    Add QR code component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-bg": 'hsl(212, 45%, 89%)',
+        "theme-blue": 'hsl(218, 44%, 22%)',
+        "theme-grey": 'hsl(220, 15%, 55%)',
+      }
+    },
   },
   plugins: [],
 }
